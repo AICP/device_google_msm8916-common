17a58e93dfcce0ec8720fe1f718b88fbbdebf63c
Kaan Külahlı <rygebis@gmail.com>
2016-11-05 18:45:02 +0000

    Switch to 32 bit

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -6,11 +6,6 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     memtrack.msm8916
 
-# Permissions
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
-endif
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -23,9 +18,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8916)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
-else
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196609
-endif
 endif
