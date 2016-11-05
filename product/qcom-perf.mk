7c7e73fe44738e5b54b3f3b23e640d26ba94f8ba
Kaan Külahlı <rygebis@gmail.com>
2016-11-05 18:46:23 +0000

    Switch to 32 bit

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -1,8 +1,3 @@
-# IRQ balance
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
-endif
 
 # Power HAL
 PRODUCT_PACKAGES += \
@@ -15,10 +10,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8916)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.min_freq_0=800000
-else
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.min_freq_0=960000 \
-    ro.min_freq_4=800000
-endif
+
 endif
