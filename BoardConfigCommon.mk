LineageOS/android_device_cyanogen_msm8916-common

msm8916: Use PLATFORM_PATH instead of VENDOR_PATH
Change-Id: I80a2a830a6f8a8480d98d7c28412a49539c7a199

BoardConfigCommon.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VENDOR_PATH := device/cyanogen/msm8916-common
+PLATFORM_PATH := device/cyanogen/msm8916-common
 
 # Include board config fragments
-include $(VENDOR_PATH)/board/*.mk
+include $(PLATFORM_PATH)/board/*.mk

AICP/device_google_msm8916-common

Conflicts:
	BoardConfigCommon.mk

BoardConfigCommon.mk
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VENDOR_PATH := device/google/msm8916-common
+
+PLATFORM_PATH := device/google/msm8916-common
 
 # Include board config fragments
-include $(VENDOR_PATH)/board/*.mk
+include $(PLATFORM_PATH)/board/*.mk